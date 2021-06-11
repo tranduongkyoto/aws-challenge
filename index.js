0df4ba7613614a4b329d9d4634e69fc360c1a422
tranduongkyoto <tranduongkyoto@gmail.com>
2021-06-11 13:28:08 +0700

    second

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
-const express = require('express');
+// call all the required packages
+const express = require('express')
 const multer = require('multer');
-const app = express()
-const port = 3000
-app.get('/', (req, res) => {
-  console.log("GET HOME", req)
-  res.sendFile('public/index.html' , { root : __dirname});
+const AWS  = require('aws-sdk');
+const S3 = require('aws-sdk/clients/s3');
+const port = 3000;
+const app = express();
+const fs = require('fs');
+const  storage = multer.diskStorage({ // storage
+  destination: function (req, file, cb) {
+    cb(null, __dirname + '/public/images')
+  },
+  filename: function (req, file, cb) {
+    const mimetype = file.mimetype.split('/')[1];
+    cb(null, file.fieldname + '-' + Date.now()+'.' + mimetype)
+  }
 })
-app.post('/upload', (req, res)=>{
-  console.log("REQ",req.body);
-  res.send("hello");
+var upload = multer({ storage: storage });
 
+const bucketName = "duong-aws"
+const region = "ap-southeast-1a"
+const accessKeyId = "AKIAQATXWFP7O4HSGPX5"
+const secretAccessKey = "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"
+
+const s3 = new S3({
+  region,
+  accessKeyId,
+  secretAccessKey
+});
+
+app.get('/', (req, res) => {
+  res.sendFile('public/home.html' , { root : __dirname})
 })
+app.post('/upload', upload.single('file'), (req, res, next) => {
+  const file = req.file;
+  console.log("FILE", req.file);
+  const filepath = fs.createReadStream(file.path);
+  const params = {
+    Bucket: bucketName, 
+    Key: `${file.filename}`,
+    Body: filepath
+  };
+  s3.putObject(params)
+    .promise()
+    .then(res => {
+      console.log(`Upload succeeded - `, res);
+    })
+    .catch(err => {
+      console.log("Upload failed:", err);
+    });
+  res.send("Success");
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
